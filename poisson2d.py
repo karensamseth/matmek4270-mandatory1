47dce8deb95959cff04dc2e0f0e7e3b099ac1419
Karen Samseth <karensam@math.uio.no>
2023-10-09 08:51:12 +0200

    Prøver å finne interpolasjonspunkter

--- poisson2d.py
+++ poisson2d.py
@@ -34,8 +34,7 @@ class Poisson2D:
     def create_mesh(self, N):
         """Create 2D mesh and store in self.xij and self.yij"""
         self.N = N
-        self.dx = self.L/N
-        self.dy = self.L/N
+        self.h = self.L/N
         x = self.x = np.linspace(0, self.L, self.N+1)
         y = self.y = np.linspace(0, self.L, self.N+1)
         self.xij, self.yij = np.meshgrid(x,y,indexing='ij')
@@ -50,8 +49,8 @@ class Poisson2D:
 
     def laplace(self):
         """Return vectorized Laplace operator"""
-        D2x = (1./self.dx**2)*self.D2()
-        D2y = (1./self.dy**2)*self.D2()
+        D2x = (1./self.h**2)*self.D2()
+        D2y = (1./self.h**2)*self.D2()
         return sparse.kron(D2x, sparse.eye(self.N+1)) + sparse.kron(sparse.eye(self.N+1),  D2y)
 
     def get_boundary_indices(self):
@@ -65,7 +64,6 @@ class Poisson2D:
         """Return assembled matrix A and right hand side vector b"""
         f = ue.diff(x, 2) + ue.diff(y, 2)
         F = sp.lambdify((x, y), f)(self.xij,self.yij)
-        print("F=\n",F)
         A = self.laplace()
         A = A.tolil()
         bnds = self.get_boundary_indices()
@@ -77,15 +75,15 @@ class Poisson2D:
         #raise RuntimeError
         b = F.ravel()
         ue_func = sp.lambdify((x,y),ue)(self.xij, self.yij).ravel()
-        print("ue=\n", ue_func.reshape((self.N+1, self.N+1)))
+        #print("ue=\n", ue_func.reshape((self.N+1, self.N+1)))
         b[bnds] = ue_func[bnds]
-        print("b=\n",b.flatten().reshape((self.N+1, self.N+1)))
+        #print("b=\n",b.flatten().reshape((self.N+1, self.N+1)))
         return A.tocsr(), b
 
     def l2_error(self, u):
         """Return l2-error norm"""
         uj = sp.lambdify([x,y], self.ue)(self.x, self.y) #evaluerer ue i meshpoints
-        return np.sqrt(self.dx*np.sum((uj-u)**2))        
+        return np.sqrt(self.h*np.sum((uj-u)**2))        
 
     def __call__(self, N): #Michael
         """Solve Poisson's equation.
@@ -103,7 +101,7 @@ class Poisson2D:
         self.create_mesh(N)
         A, b = self.assemble()
         self.U = sparse.linalg.spsolve(A, b.flatten()).reshape((N+1, N+1))
-        print("U=\n",self.U.reshape((N+1,N+1)))
+        #print("U=\n",self.U.reshape((N+1,N+1)))
         return self.U
 
     def convergence_rates(self, m=6): #Michael
@@ -145,9 +143,80 @@ class Poisson2D:
         The value of u(x, y)
 
         """
-        ix = np.where(self.x == x_coord)[0]
-        iy = np.where(self.y == y_coord)[0]
-        return self.U[ix,iy]
+        # Finner punktene som er rundt (x_coord,y_coord): (tilsvarende for x og y)
+        print("h=",self.h)
+        print("evalueringspunkt:",x_coord,y_coord)
+        for xi in self.x: #x-koord.
+            if x_coord-xi<self.h and x_coord-xi>0:
+                x_lower = xi
+            elif x_coord-xi==self.h:
+                break
+            elif -(x_coord-xi)<self.h and -(x_coord-xi)>0:
+                x_upper = xi
+        for yi in self.y: #y-koord
+            if y_coord-yi<self.h and y_coord-yi>0:
+                y_lower = yi
+            elif y_coord-yi==self.h:
+                break
+            elif -(y_coord-yi)<self.h and -(y_coord-yi)>0:
+                y_upper = yi
+        print("x ligger mellom punktene ",x_lower,x_upper)
+        print("y ligger mellom punktene ",y_lower,y_upper)
+        x_interppunkter = [x_lower-self.h, x_lower, x_upper,x_upper+self.h]
+        ix_lower = np.where(self.x == x_lower)[0]
+        ix_upper = np.where(self.x == x_upper)[0]
+        if x_interppunkter[0] < min(self.x):
+            x_interppunkter[0] = self.x[ix_upper+2]
+        else:
+            x_interppunkter[0] = self.x[ix_lower-1]
+        if x_interppunkter[-1] > max(self.x):
+            x_interppunkter[-1] = self.x[ix_lower-2]
+        else:
+            x_interppunkter[-1] = self.x[ix_upper+1]
+        x_interppunkter = sorted(x_interppunkter)
+        print("x sortert:", x_interppunkter)
+        y_interppunkter = [y_lower-self.h, y_lower, y_upper,y_upper+self.h]
+        iy_lower = np.where(self.y == y_lower)[0]
+        iy_upper = np.where(self.y == y_upper)[0]
+        if y_interppunkter[0] < min(self.y):
+            y_interppunkter[0] = self.y[iy_upper+2]
+        else:
+            y_interppunkter[0] = self.y[iy_lower-1]
+        if y_interppunkter[-1] > max(self.y):
+            y_interppunkter[-1] = self.y[iy_lower-2]
+        else:
+            y_interppunkter[-1] = self.y[iy_upper+1]
+        y_interppunkter = sorted(y_interppunkter)
+        u_interppunkter = [self.U[ix_lower-1,iy_lower-1], self.U[ix_lower,iy_lower],\
+                           self.U[ix_upper,iy_upper], self.U[ix_upper+1,iy_upper+1]]
+        print("u-verdier:", u_interppunkter)
+        # Lagrange basis: (lik for x og y)
+        nx = len(x_interppunkter)
+        ellx = []
+        numertx = sp.Mul(*[x-x_interppunkter[i] for i in range(nx)])
+        for i in range(nx):
+            numer = numertx/(x-x_interppunkter[i])
+            denom = sp.Mul(*[(x_interppunkter[i]-x_interppunkter[j]) for j in range(nx) if i!=j])
+            ellx.append(numer/denom)
+        basisx = ellx
+        ny = len(y_interppunkter)
+        elly = []
+        numerty = sp.Mul(*[y-y_interppunkter[i] for i in range(ny)])
+        for i in range(ny):
+            numer = numerty/(y-y_interppunkter[i])
+            denom = sp.Mul(*[(y_interppunkter[i]-y_interppunkter[j]) for j in range(ny) if i!=j])
+            elly.append(numer/denom)
+        basisy = elly
+        # Lagrange interpolating polynomial:
+        N, M = self.U.shape
+        f = 0
+        for i in range(N):
+            for j in range(M):
+                f += basisx[i]*basisy[i]*u_interppunkter[i,j]
+        print("f",f)
+        # Using Lagrange to evaluate point:
+        #gjør om f til en funksjon og evaluer i x_coord
+        return print("ikke ferdig")
 
 def test_convergence_poisson2d(): #Michael
     # This exact solution is NOT zero on the entire boundary
@@ -164,7 +233,17 @@ def test_interpolation(): #Michael
     assert abs(sol.eval(sol.h/2, 1-sol.h/2) - ue.subs({x: sol.h, y: 1-sol.h/2}).n()) < 1e-3
 
 if __name__ == '__main__':
-    ue = x**3
-    sol = Poisson2D(10, ue)
-    U = sol(10)
-    print(sol.eval(2,2))
+    """
+    ue = sp.exp(sp.cos(4*sp.pi*x)*sp.sin(2*sp.pi*y))
+    sol = Poisson2D(1, ue)
+    r, E, h = sol.convergence_rates()
+    K = abs(r[-1]-2) < 1e-2
+    print(K)
+    """
+    ue = sp.exp(sp.cos(4*sp.pi*x)*sp.sin(2*sp.pi*y))
+    sol = Poisson2D(1, ue)
+    U = sol(100)
+    K2 = abs(sol.eval(sol.h/2, 1-sol.h/2) - ue.subs({x: sol.h, y: 1-sol.h/2}).n()) < 1e-3
+    print(K2,"Hvis denne er tom må du interpolere!!")
+    #K1 = abs(sol.eval(0.52, 0.63) - ue.subs({x: 0.52, y: 0.63}).n()) < 1e-3
+    #print("K1=", K1)
